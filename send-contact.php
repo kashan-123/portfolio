1c389e5bd273afdaf7d88401cad41c170cdc9523
kashan-123 <kashansaifi52@gmail.com>
2026-01-20 15:01:06 +0530

    Add files via upload
    ddf

--- send-contact.php
+++ send-contact.php
@@ -1,4 +1,3 @@
-
 <?php
 
 use PHPMailer\PHPMailer\PHPMailer;
@@ -6,6 +5,8 @@ use PHPMailer\PHPMailer\Exception;
 
 require __DIR__ . '/vendor/autoload.php';
 
+
+// Load SMTP settings
 $env = parse_ini_file(__DIR__ . '/.env');
 
 if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
@@ -22,8 +23,7 @@ if (!$name || !$email || !$subject || !$message) {
 }
 
 try {
-
-    /* ================= ADMIN MAIL ================= */
+    /* ================= ADMIN EMAIL ================= */
     $mail = new PHPMailer(true);
     $mail->isSMTP();
     $mail->Host       = $env['MAIL_HOST'];
@@ -31,24 +31,26 @@ try {
     $mail->Username   = $env['MAIL_USERNAME'];
     $mail->Password   = $env['MAIL_PASSWORD'];
     $mail->SMTPSecure = $env['MAIL_ENCRYPTION'];
-    $mail->Port       = $env['MAIL_PORT'];
+    $mail->Port       = (int)$env['MAIL_PORT'];
 
     $mail->setFrom($env['MAIL_FROM_ADDRESS'], $env['MAIL_FROM_NAME']);
     $mail->addAddress($env['ADMIN_EMAIL']);
 
     $mail->isHTML(true);
-    $mail->Subject = 'New Portfolio Contact Message';
+    $mail->Subject = "New Portfolio Contact Message";
     $mail->Body = "
-        <h3>New Contact Enquiry</h3>
+        <h2>New Contact Enquiry</h2>
         <p><b>Name:</b> {$name}</p>
         <p><b>Email:</b> {$email}</p>
         <p><b>Subject:</b> {$subject}</p>
-        <p><b>Message:</b><br>{$message}</p>
+        <p><b>Message:</b><br>" . nl2br($message) . "</p>
+        <hr>
+        <p>Portfolio Contact Form</p>
     ";
 
     $mail->send();
 
-    /* ================= USER AUTO-REPLY ================= */
+    /* ================= USER AUTO REPLY ================= */
     $reply = new PHPMailer(true);
     $reply->isSMTP();
     $reply->Host       = $env['MAIL_HOST'];
@@ -56,24 +58,39 @@ try {
     $reply->Username   = $env['MAIL_USERNAME'];
     $reply->Password   = $env['MAIL_PASSWORD'];
     $reply->SMTPSecure = $env['MAIL_ENCRYPTION'];
-    $reply->Port       = $env['MAIL_PORT'];
+    $reply->Port       = (int)$env['MAIL_PORT'];
 
     $reply->setFrom($env['MAIL_FROM_ADDRESS'], $env['MAIL_FROM_NAME']);
     $reply->addAddress($email, $name);
 
     $reply->isHTML(true);
-    $reply->Subject = 'Thanks for contacting me';
+    $reply->Subject = "Thanks for contacting me";
     $reply->Body = "
-        <p>Hi {$name},</p>
-        <p>Thank you for contacting me. I have received your message and will reply soon.</p>
+    <p>Hi {$name},</p>
+
+    <p>
+        Thank you for reaching out. I’ve received your message and will respond as soon as possible.
+    </p>
+
+    <p>
+        You can also check out my work here:
         <br>
-        <p>Regards,<br>{$env['MAIL_FROM_NAME']}</p>
-    ";
+        🔗 <a href='https://kashan-123.github.io/portfolio/' target='_blank'>
+        Visit My Portfolio</a>
+    </p>
+
+    <br>
+
+    <p>
+        Best regards,<br>
+        <strong>{$env['MAIL_FROM_NAME']}</strong>
+    </p>
+";
+
 
     $reply->send();
 
-    echo 'OK';
-
+    echo "OK";
 } catch (Exception $e) {
-    echo 'Mail Error: ' . $e->getMessage();
+    echo "Mail Error: " . $e->getMessage();
 }
